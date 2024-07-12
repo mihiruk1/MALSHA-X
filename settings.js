247b1b328b7b6d42dbbdf2e101a543a725297e0c
mihiruk1 <154600797+mihiruk1@users.noreply.github.com>
2024-07-12 23:39:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＭＡＬＳＨＡ Ｘ -  ＭＤ - Ｖ1';
 global.owner = process.env.OWNER_NUMBER  || '94766969406' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=fd9nFCBT#yfO-qMl8105dBoe5nS3zFW_cEC8yYki8wHtBYHDZhys' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94778764083,94701073037" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
